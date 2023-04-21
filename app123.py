08f75e91aa0eb4367f8d339d326e5c51ad906293
janhavighuge <42887133+janhavighuge@users.noreply.github.com>
2023-04-21 00:23:13 -0400

    Update app123.py

--- app123.py
+++ app123.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+# increase the timeout settings
+import time
+import signal
+
+def timeout_handler(signum, frame):
+    raise TimeoutError()
+
+signal.signal(signal.SIGALRM, timeout_handler)
+signal.alarm(30) # set timeout to 30 seconds
+
+# define the routes
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return '<h1>FLIPP</h1><p>This app allows you to detect the emotion of your text, video and audio inputs.</p>'
+
+# disable the timeout alarm
+signal.alarm(0)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(port=8000)
